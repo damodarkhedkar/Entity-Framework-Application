47c910c45d4c71ea11019b1e75754c32848c0aff
damodarkhedkar <36823228+damodarkhedkar@users.noreply.github.com>
2020-02-20 16:43:21 +0530

    Initial commit

---
+++ Entity Framework Application/EmplopyeeContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Entity_Framework_Application
+{
+    class EmplopyeeContext : DbContext
+    {
+       public DbSet<Employee> Employees { get; set; } 
+    }
+}
---
+++ Entity Framework Application/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entity_Framework_Application
+{
+    class Employee
+    {
+        [Key]
+        public int Eid {set; get;}
+        public string Ename { set; get; }
+        public string Designation { set; get; }
+        public double Salary { set; get; }
+    }
+}
